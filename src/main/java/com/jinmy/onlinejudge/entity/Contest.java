a6fe488ba5af997c54f096f6ff6cdec56799ba08
jinmingyi1998 <jinmingyi1998@sina.cn>
2019-03-13 21:40:20 +0800

    contest类新增User creator

--- src/main/java/com/jinmy/onlinejudge/entity/Contest.java
+++ src/main/java/com/jinmy/onlinejudge/entity/Contest.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package com.jinmy.onlinejudge.entity;
 
 import lombok.Data;
@@ -31,7 +39,8 @@ public class Contest {
     private Instant startTime;
     @Column(nullable = false)
     private Instant endTime;
-
+    @ManyToOne
+    private User creator;
     @Column(nullable = false)
     private Instant createTime;
     @OneToMany(mappedBy = "contest")
