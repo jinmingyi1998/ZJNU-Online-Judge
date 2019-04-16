fadf4734bd650f3ca1bff6a49f70d6b6f7b431d8
Jimmy_King <jinmingyi1998@sina.cn>
2019-04-16 23:49:33 +0800

    负责文件管理，实现了解压zip功能，在服务器启动时检查所需目录是否存在，不存在则创建

---
+++ src/main/java/com/jinmy/onlinejudge/util/DataManager.java
@@ -0,0 +1,93 @@
+package com.jinmy.onlinejudge.util;
+
+import com.jinmy.onlinejudge.config.Config;
+import com.jinmy.onlinejudge.config.JudgeSwitchConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.*;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+@Slf4j
+@Component
+public class DataManager {
+
+    @Autowired
+    Config config;
+    @Autowired
+    JudgeSwitchConfig judgeSwitchConfig;
+
+    @PostConstruct
+    public void init() {
+        if (judgeSwitchConfig.getAutoCreateDir()) {
+            initCreating(config.getRootDir());
+            initCreating(config.getSrcDir());
+            initCreating(config.getDataDir());
+            initCreating(config.getTmpDir());
+            initCreating(config.getOutDir());
+        }
+    }
+
+    private void initCreating(String filename) {
+        File file = new File(filename);
+        if (file.exists()) {
+            log.info(filename + " Existed");
+        } else {
+            if (file.mkdirs())
+                log.info(filename + " not existed, created");
+            else
+                log.error(filename + " not existed, created fail");
+        }
+
+    }
+
+    public DataManager() {
+    }
+
+    public void unZipFiles(File file, String dir) throws IOException {
+        ZipFile zipFile = new ZipFile(file);
+        Enumeration zlist = zipFile.entries();
+        while (zlist.hasMoreElements()) {
+            ZipEntry entry = (ZipEntry) zlist.nextElement();
+            String entryName = entry.getName();
+            InputStream inputStream = zipFile.getInputStream(entry);
+            String outPath = dir + entryName;
+            if (new File(outPath).isDirectory()) {
+                continue;
+            }
+            log.info("Unzip File: " + outPath);
+            OutputStream outputStream = new FileOutputStream(outPath);
+            byte[] buf1 = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buf1)) != -1) {
+                outputStream.write(buf1, 0, len);
+            }
+            inputStream.close();
+            outputStream.close();
+        }
+    }
+
+    public void moveFiles() {
+    }
+
+    public void deleteFiles() {
+    }
+
+    public void uploadDataFromZip(File file, String destDir) throws IOException {
+        File dir = new File(destDir);
+        if (!dir.exists()) {
+            dir.mkdir();
+        } else {
+            for (int i = 0; i < dir.list().length; i++) {
+                File f = new File(dir.list()[i]);
+                if (!f.isDirectory())
+                    f.delete();
+            }
+        }
+        unZipFiles(file, destDir);
+    }
+}
